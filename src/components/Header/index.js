39730d3212b4114f03aaa444032f858f99722e60
Mosbycr <mosbycr@gmail.com>
2019-06-21 19:29:14 -0400

    imported react, started header index.js, adding in bootstrap

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,20 @@
 import React from "react";
 
 function Header() {
-    return(
-        <div></div>
-    )
+  return (
+    <header className="header">
+      <div className="jumbotron">
+        <h1 className="display-4">React Memory Image Game</h1>
+        <p className="lead">
+          Click on an image to start the game and earn points, but click on the
+          same image twice and the game is over.
+        </p>
+        <p className="score-board">
+          <span />
+        </p>
+      </div>
+    </header>
+  );
 }
-export default Header
+
+export default Header;
