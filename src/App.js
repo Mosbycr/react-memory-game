451cd23d57e3e2da4fc0dfba38307db25ce1738f
Mosbycr <mosbycr@gmail.com>
2019-06-22 07:51:25 -0400

    created ImageCard with props, Starting to add json for images with id, image src, and clicked, added imageCard into app with map to loop through json

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from "react";
 import ImageCard from "./components/ImageCard";
 import Header from "./components/Header";
 import Wrapper from "./components/Wrapper";
-import imageCards from imageCards.json;
+import imageCards from "./imageCards.json";
 
 class App extends Component {
     state = {
@@ -13,6 +13,15 @@ class App extends Component {
         return (
             <Wrapper>
                 <Header />
+                {this.state.imageCards.map(image => (
+                    <ImageCard 
+                    id={image.id}
+                    key={image.id}
+                    image={image.image}
+                    clicked={image.clicked}
+                    />
+
+                ))}
             </Wrapper>
         )
     }
