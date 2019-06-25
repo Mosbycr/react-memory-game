cead224ffc22e2d7eb5d6898cca780c2f74b6fe9
Mosbycr <mosbycr@gmail.com>
2019-06-25 16:47:10 -0400

    added css to jumbotron

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,12 +7,11 @@ function Header(props) {
       <div className="jumbotron">
         <h1 className="display-4">React Memory Image Game</h1>
         <p className="lead">
-          Click on an image to start the game and earn points, but click on the
-          same image twice and the game is over. Keep trying to get all 12 images!
+          <em>Click on an image to start the game and earn points, but click on the
+          same image twice and the game is over. <br/> Keep trying to get all 12 images!</em>
         </p>
         <p className="score-board">
-          Score: {props.score}   |   TopScore: {props.topScore} 
-      
+         <b> Score: {props.score}   |   TopScore: {props.topScore} </b>
         </p>
       </div>
     </header>
