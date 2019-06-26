e6d87871c8b1c7d23429e069d9883b345eb46066
Mosbycr <mosbycr@gmail.com>
2019-06-25 21:52:10 -0400

    Updates

---
+++ precache-manifest.6cf2920e90655bbaecc01f05ec77451c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5c877805645bbe9dc885ebff15a7a8f",
+    "url": "/react-memory-game/index.html"
+  },
+  {
+    "revision": "d89496a05ed86dec23e9",
+    "url": "/react-memory-game/static/css/main.33b1cdf8.chunk.css"
+  },
+  {
+    "revision": "00063f64c68494124901",
+    "url": "/react-memory-game/static/js/2.49afd6f5.chunk.js"
+  },
+  {
+    "revision": "d89496a05ed86dec23e9",
+    "url": "/react-memory-game/static/js/main.5dca80b1.chunk.js"
+  },
+  {
+    "revision": "e8b71f681a914c951703",
+    "url": "/react-memory-game/static/js/runtime~main.83a7a993.js"
+  }
+]);
